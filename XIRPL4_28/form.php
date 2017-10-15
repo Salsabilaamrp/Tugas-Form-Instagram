30e624ef336e8d265a37cc44d757eb821dc11e6c
Salsabilaamrp <30397545+Salsabilaamrp@users.noreply.github.com>
2017-10-15 23:25:16 +0700

    Tugas Form Instagram
    Salsabila Amara Putri XI RPL 4(28) Tugas PK5

---
+++ XIRPL4_28/form.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<center><br/><br/>
+<img width=225 height=100 src='image/logo.jpg'/><br/><br/>
+<font size=5 font color="#999999"> Buat akun untuk melihat foto dan <br/> video dari teman Anda.</font></font><br/><br/>
+<img width=25 height=15 src='image/fb.png'/>
+Masuk dengan Facebook<br/><br/>
+
+
+<table> 
+<tr>
+<td> ------------ </td>
+<td> ATAU </td> 
+<td> ------------ </td>
+</tr>
+</table><br/>
+
+
+<form action="hasil.php" method=POST>
+<table>
+
+<tr>
+<td>Email</td>
+<td>:</td>
+<td><input name="email" Size="25" type="email"></td>
+</tr>
+<tr>
+<td>Fullname</td>
+<td>:</td>
+<td><input name="fullname" Size="25" type="text"></td>
+</tr>
+<tr>
+<td>Username</td>
+<td>:</td>
+<td><input name="username" Size="25" type="text"></td>
+</tr>
+<tr>
+<td>Password</td>
+<td>:</td>
+<td><input name="password" Size="25" type="password"></td>
+</tr>
+<tr><td><br/></td></tr>
+</table>
+
+<table>
+<tr>
+<td> <input type=submit value="Sign Up">
+</td>
+<td></td>
+<td> <input type=reset value="Clear" /></td>
+</tr>
+</table><br/><br/>
+<font face="Arial">
+Dengan mendaftar, Anda menyetujui<br/>
+<b>Ketentuan & Kebijakan Privasi</b> kami.</font>
+<br/><br/><br/>
+</form>
+
+Punya akun? Masuk
+
+
+</center>
+
+</body>
+</html>
